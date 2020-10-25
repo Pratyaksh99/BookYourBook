ab892919f368a4e385bcc0fdfee1e0de4525c78f
Kaushik <ramach10@purdue.edu>
2020-10-24 22:42:41 -0400

    Hosted Application Modifications

--- FlaskApp/main.py
+++ FlaskApp/main.py
@@ -1,15 +1,6 @@
-from flask import Flask, render_template
-from flaskext.mysql import MySQL
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-mysql = MySQL()
-
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Kismish**1'
-app.config['MYSQL_DATABASE_DB'] = 'BookYourBook'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
 
 @app.route("/")
 def main():
@@ -36,4 +27,4 @@ def showBookList():
     return render_template('bookList.html')
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='127.0.0.1', port=8080, debug=True)
